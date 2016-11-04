f53f4fe7c70ef21fcd1e2563b8eae497e862fb51
d2fong <d2fong@uwaterloo.ca>
2016-11-04 14:58:32 -0400

    Adds error handling, bounds checking for the input

--- src/kaprekar/index.js
+++ src/kaprekar/index.js
@@ -2,13 +2,25 @@ var app = new Vue({
   el: '#app',
   data: {
     rangeStart: 1,
-    rangeEnd: 50
+    rangeEnd: 50,
+    error: ''
   },
   methods: {
     computeKaprekar: function() {
       var start = this.rangeStart;
       var end = this.rangeEnd;
 
+      if (start < 1 || end < 1) {
+        this.error = 'Numbers must be >= 0';
+        return;
+      }
+
+      if (end > 100000) {
+        this.error = 'Please do not input that large of a number, I will crash =(';
+        return;
+      }
+
+      this.error = ''
       return kaprekarNumberChallenge(start, end)
     }
   }
